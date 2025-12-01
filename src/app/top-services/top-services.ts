06613b1deaaa7bfd9ec5cf1e58012e3c1219584d
Rahul <romeodm17@gmail.com>
2025-12-02 02:42:19 +0530

    lead-form

--- src/app/top-services/top-services.ts
+++ src/app/top-services/top-services.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-top-services',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './top-services.html',
   styleUrl: './top-services.css',
 })
-export class TopServices {
-
-}
+export class TopServices {}
